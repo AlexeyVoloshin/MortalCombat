25a9c1cfca79536d00eca885d6a71aaeeedb59b7
Alexey Voloshin <voloshinalex1100@gmail.com>
2021-04-10 21:24:58 +0300

    add random hit

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const $arenas = document.querySelector('.root .arenas');
+const $randomButton = document.querySelector('.button');
+let $winsTitle;
 
 const player1 = {
 	player: 1,
 	name: 'SCORPION',
-	hp: 50,
+	hp: 100,
 	img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
 	weapon: ['ax', 'harpoon', 'gun'],
 	attack: function() {
@@ -13,9 +15,9 @@ const player1 = {
 
 const player2 = {
 	player: 2,
-	name: 'KITANA',
-	hp: 80,
-	img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
+	name: 'LIUKANG',
+	hp: 100,
+	img: 'http://reactmarathon-api.herokuapp.com/assets/liukang.gif',
 	weapon: ['ax', 'harpoon', 'gun'],
 	attack: function() {
 		console.log(this.name + ' Fight...');
@@ -51,5 +53,64 @@ function createPlayer(character) {
 
 	return $player;
 };
+
+function changeHP(player, randomNum, healthLevel) {
+	const $playerLife = document.querySelector('.player'+player.player+ ' .life');
+/* 
+	данная проверка исключает запись отрицательного числа в player.hp, 
+	что не давало зайти в условие player.hp <= 0 c первого раза. 
+*/
+	if (player.hp - randomNum <= 0) {
+		player.hp = 0;
+		$playerLife.style.width = player.hp + '%';
+		if (player.hp === 0 && healthLevel === 0) {
+			$arenas.appendChild(bothLost());
+			return;
+		}
+		if (player.player === 1) {
+			$arenas.appendChild(playerWin(player2.name));
+		} else {
+			$arenas.appendChild(playerWin(player1.name));
+		};
+	} else {
+		player.hp -= randomNum;
+		$playerLife.style.width = player.hp + '%';
+	};
+};
+
+function playerWin(name) {
+	$winsTitle = createElement('div', 'winsTitle');
+	$winsTitle.innerText = name + ' wins';
+	$randomButton.disabled = true;
+
+	return $winsTitle;
+};
+
+function bothLost() {
+	const $bothLostTitle = createElement('div', 'bothLostTitle');
+	/* 
+		Удаляю дом ноду "winsTitle", потому что не смог решить проблему с добавлением winsTitle 
+		когда у одного из игроков hp == 0 а второму еще не проверил уровень его hp;
+	*/
+	if ($winsTitle !== null) {
+		$arenas.removeChild($winsTitle);
+	};
+	$bothLostTitle.innerText = 'Both Lost';
+	$randomButton.disabled = true;
+
+	return $bothLostTitle;
+};
+
+function generateRandomNum(min, max) {
+	const number = Math.ceil(Math.random() * (max - min + 1)) + min;
+
+	return number;
+};
+
+$randomButton.addEventListener('click', function() {
+	changeHP(player1, generateRandomNum(1, 20), player2.hp);
+	changeHP(player2, generateRandomNum(1, 20), player1.hp);
+});
+
 $arenas.appendChild(createPlayer(player1));
 $arenas.appendChild(createPlayer(player2));
